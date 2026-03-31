736b687d055c421a4cc77141d518014fdc3bc21c
bahaell <bahaaellouze@gmail.com>
2026-03-31 20:44:47 +0100

    feat: setup notification-service foundation (STEP 1)

---
+++ notification-service/src/main/java/com/pfe/notification/config/FirebaseConfig.java
@@ -0,0 +1,14 @@
+package com.pfe.notification.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Placeholder for Firebase Configuration
+ * To be implemented in later steps.
+ */
+@Configuration
+public class FirebaseConfig {
+
+    // TODO: Implement Firebase Admin SDK initialization
+
+}
